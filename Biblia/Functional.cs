3aad55562beb67f498c9daac77a55d0e55954ddd
Андрей Кочнев <andrej.kochnev.2004@inbox.ru>
2023-04-28 21:28:45 +0300

    Добавьте файлы проекта.

---
+++ Biblia/Functional.cs
@@ -0,0 +1,189 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace Biblia
+{
+    internal class Functional
+    {
+        int rowCount;
+        int colCount;
+        Excel.Application excelApp = new Excel.Application();//создаем объект приложения
+        string pathFile = Environment.CurrentDirectory + "\\list.xlsx";
+
+        Workbook ExcelBook;
+        public Functional()
+        {
+
+        }
+
+        /// <summary>
+        /// Открытие файла
+        /// </summary>
+        /// <param name="numberList">Номер листа</param>
+        /// <returns>Данные</returns>
+        Range fileAccess(int numberList)
+        {
+            ExcelBook = excelApp.Workbooks.Open(pathFile);
+            _Worksheet worksheet = (Worksheet)ExcelBook.Worksheets[numberList];//выбираем лист
+            Range excelRange = worksheet.UsedRange;//найти используемые ячейки в массиве
+            return excelRange;
+        }
+
+        /// <summary>
+        /// Вывод должников
+        /// </summary>
+        public void conclusionReaders()
+        {
+            Range excelRange = fileAccess(1);
+            rowCount = excelRange.Rows.Count;
+            colCount = excelRange.Columns.Count;
+
+            for (int i = 2; i <= rowCount; i++)
+            {
+                for (int j = 1; j <= colCount; j++)
+                {                   
+                    if ((excelRange.Cells[i, j] != null) && (excelRange.Cells[i, j].Value2 != null) && (excelRange.Cells[i, 6].Value2 == null))
+                    { 
+                        Console.Write(excelRange.Cells[i, j].Value2.ToString() + "\t");
+                        if (j == (colCount - 1))
+                        {
+                            Console.Write("\r\n");
+                        } 
+                    }
+                    
+                }
+            }      
+            excelApp.Quit();
+        }
+
+        /// <summary>
+        /// Вывод книг
+        /// </summary>
+        public void bookList()
+        {
+            Range excelRange = fileAccess(2);
+            rowCount = excelRange.Rows.Count;
+            colCount = excelRange.Columns.Count;
+            for (int i = 1; i <= rowCount; i++)
+            {
+                for (int j = 1; j <= colCount; j++)
+                {
+                    if (excelRange.Cells[i, j].Value2 == null)
+                    {
+                        Console.Write("\t");
+                    }
+                    if ((excelRange.Cells[i, j] != null) && (excelRange.Cells[i, j].Value2 != null))
+                    {
+                        Console.Write(excelRange.Cells[i, j].Value2.ToString() + "\t");                      
+                    }
+                }
+             Console.WriteLine();
+            }
+            excelApp.Quit();
+        }
+
+        /// <summary>
+        /// Запись о возврате
+        /// </summary>
+        public void returnMarkBook()
+        {
+            Range excelRange = fileAccess(1);
+            rowCount = excelRange.Rows.Count;
+            colCount = excelRange.Columns.Count;
+            Console.WriteLine("Введите ФИО читателя");
+            string reader = Console.ReadLine();
+
+            for (int i = 1; i <= rowCount; i++)
+            {
+                for (int j = 1; j <= colCount; j++)
+                {                   
+                    if ((excelRange.Cells[i, j] != null) && (excelRange.Cells[i, j].Value2 != null) && (excelRange.Cells[i,2].Value2==reader) && (excelRange.Cells[i, 6].Value2 == null))
+                    {
+                        Console.Write(excelRange.Cells[i, j].Value2.ToString() + "\t");
+                        if (j == (colCount - 1))
+                        {
+                            Console.Write("\r\n");
+                            Console.WriteLine("Введите дату возврата (пример: 10 10 2010)");
+                            excelRange.Cells[i, 6] = Console.ReadLine();
+                            ExcelBook.Save();
+                        }                       
+                    }
+                }
+            }
+            excelApp.Quit();
+        }
+
+        /// <summary>
+        /// Новая запись о взятии книг
+        /// </summary>
+        public void addingNewEntry()
+        {
+            Range excelRange = fileAccess(1);
+            rowCount = excelRange.Rows.Count;
+            colCount = excelRange.Columns.Count;
+            for (int i = 1; i < colCount; i++)
+            {
+                switch(i)
+                {
+                    case 1:                        
+                            Console.WriteLine("Введите фамилию библиотекаря");
+                            break;                       
+                    case 2:                       
+                            Console.WriteLine("Введите ФИО читателя");
+                            break;                       
+                    case 3:                        
+                            Console.WriteLine("Введите название книги");
+                            break ;                      
+                    case 4:                       
+                            Console.WriteLine("Введите автора");
+                            break;                        
+                    case 5:                        
+                            Console.WriteLine("Введите дату взятия (пример: 10 10 2010)");
+                            break;                                          
+                        
+                }
+                excelRange.Cells[rowCount+1, i] = Console.ReadLine();
+            }
+            ExcelBook.Save();
+            excelApp.Quit();
+        }
+
+        /// <summary>
+        /// Новая запись  книг
+        /// </summary>
+        public void addingBook()
+        {
+            Range excelRange = fileAccess(2);
+            rowCount = excelRange.Rows.Count;
+            colCount = excelRange.Columns.Count;
+            for (int i = 1; i < colCount+1; i++)
+            {
+                switch (i)
+                {
+                    case 1:
+                        Console.WriteLine("Введите автора");
+                        break;
+                    case 2:
+                        Console.WriteLine("Введите название произведения");
+                        break;
+                    case 3:
+                        Console.WriteLine("Введите издательство");
+                        break;
+                    
+
+                }
+                excelRange.Cells[rowCount + 1, i] = Console.ReadLine();
+            }
+            ExcelBook.Save();
+            excelApp.Quit();
+        }
+    }
+}
---
+++ Biblia/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Biblia
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Functional fun = new Functional();
+            int answer;
+            while (true)
+            {
+                int k = 0;
+                Console.WriteLine("\n1-Просмотр списка читателей, не вернувших книгу\n" +
+                "2- Посмотреть перечень книг в библиотеке\n" +
+                "3- Добавить новую запись\n" +
+                "4- Отметить возврат книги\n" +
+                "5- Добавить новую книгу в архив\n");
+                try
+                {
+                    answer = Convert.ToInt32(Console.ReadLine());
+                    switch (answer)
+                    {
+                        case 1:
+                            fun.conclusionReaders();
+                            break;
+                        case 2:
+                            fun.bookList();
+                            break;
+                        case 3:
+                            fun.addingNewEntry();
+                            break;
+                        case 4:
+                            fun.returnMarkBook();
+                            break;
+                        case 5:
+                            fun.addingBook(); 
+                            break;
+                        default: 
+                            k++; 
+                            break;
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Введите корректное значение!");
+                }
+                if(k != 0)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine("Программа завершила свою работу!");
+            Console.ReadLine();
+        }
+    }
+}
